efaf909915fb1826a5d67ea3899fb0866e7273ba
hypatia-of-sva <121448956+hypatia-of-sva@users.noreply.github.com>
2026-02-07 20:58:15 +0100

    Update run_on_wavs.sh

--- run_on_wavs.sh
+++ run_on_wavs.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+#see https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    PROGRAMNAME=praat
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+    PROGRAMNAME=Praat
+    export PATH=$PATH:/Applications/Praat.app/Contents/macOS
+#elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+#elif [[ "$OSTYPE" == "msys" ]]; then
+#        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+#elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+#elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+        echo "Only Linux and MacOS currently tested and supported!"
+        exit
+fi
+
+
 for NAME in *.wav
 do
 
@@ -22,8 +44,8 @@ Get mean: 0, 0, \"Hertz\"
 
 
     
-    echo -n "F0(Hz) " >> $FILENAME
-    praat --run temp.praat >> $FILENAME
+    printf "F0(Hz) " >> $FILENAME
+    $PROGRAMNAME --run temp.praat >> $FILENAME
     rm temp.praat
 
 
@@ -36,40 +58,40 @@ To Formant (burg): 0, 5, 5500, 0.025, 50
 List: \"yes\", \"no\", 3, \"no\", 2, \"no\", 2, \"yes\"
 """ > temp.praat
 
-    praat --run temp.praat > temp.txt
+    $PROGRAMNAME --run temp.praat > temp.txt
     rm temp.praat
     
     
-    echo -n "F1(Hz) " >> $FILENAME
+    printf "F1(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=2 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
-    echo -n "B1(Hz) " >> $FILENAME
+    printf "B1(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=3 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
-    echo -n "F2(Hz) " >> $FILENAME
+    printf "F2(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=4 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
-    echo -n "B2(Hz) " >> $FILENAME
+    printf "B2(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=5 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
-    echo -n "F3(Hz) " >> $FILENAME
+    printf "F3(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=6 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
-    echo -n "B3(Hz) " >> $FILENAME
+    printf "B3(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=7 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
     
-    echo -n "F4(Hz) " >> $FILENAME
+    printf "F4(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=8 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
-    echo -n "B4(Hz) " >> $FILENAME
+    printf "B4(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=9 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
     
-    echo -n "F5(Hz) " >> $FILENAME
+    printf "F5(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=10 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
-    echo -n "B5(Hz) " >> $FILENAME
+    printf "B5(Hz) " >> $FILENAME
     cat temp.txt | tail --lines=+2 | less | awk -v N=11 '{ sum += $N } END { if (NR > 0) print sum / NR }' >> $FILENAME
     
     
